a1bf32543d3ea3206bd7bada4bae79dd29b8816c
chilaxan <35645806+chilaxan@users.noreply.github.com>
2018-08-31 10:18:42 -0400

    Create gba.js

---
+++ Bookmarklet Sources/gba.js
@@ -0,0 +1,19 @@
+var win=window.open(
+     "about.blank",
+     "JSEmbed",
+     "resizeable,scrollbars"
+);
+win.document.write('
+  <style>
+    .frame-area {
+        display: block;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        border: 0px;
+        margin: 0px;
+        padding: 0p;
+    }
+    </style>
+    <iframe class="frame-area" src="https://chilaxan.github.io/gba/index.html" gesture="media"  allow="encrypted-media" allowfullscreen></iframe>
+');
